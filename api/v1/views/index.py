eac5c31d98b71a002d239926a225ecf5e0b313ae
Kimathi isaac <Isaackimathi@gmail.com>
2024-03-08 15:52:46 +0300

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,14 +3,24 @@
 
 from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
     """status of API"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
 
 @app.errorhandler(404)
 def not_found(error):
     """404 error handling"""
     return make_response(jsonify({'error': "Not found"}), 404)
-
-
